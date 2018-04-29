98f88d8f6ebd0d739d1a60686f39dd7a6ae7b8f1
Belhassen <Belhassen.Gharsallah@isetrades.com>
2018-04-29 15:11:50 +0200

    20% style Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,8 +5,30 @@ const Header = () => (
   <div className="position-relative header">
     <header className="Header">
       <div className="display-flex padding-x-3 container-g">
-        <div className="display-flex">
+        <div className="display-flex logo-box">
           <img src={logo} className="logo"/>
+        </div>
+
+        <div className="display-flex flex-auto flex-justify-between">
+          <div className="display-flex">
+            <div className="header-search">
+              <form role="search" className="position-relative">
+                <label className="form-control header-search-wrapper">
+                  <input
+                    type="text"
+                    className="form-control header-search-input"
+                    placeholder="search smokers"
+                    autocapitalize="off"
+                  />
+                </label>
+              </form>
+            </div>
+
+          </div>
+
+          <div className="display-flex">
+
+          </div>
         </div>
       </div>
     </header>
